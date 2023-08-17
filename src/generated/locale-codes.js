8fde3c0e7497370e1e1bbfe8e569f4dcb2e8918b
Yusril Adriansyah P <yusriladr.37@gmail.com>
2023-08-17 19:33:40 +0700

    fix lint

--- src/generated/locale-codes.js
+++ src/generated/locale-codes.js
@@ -4,22 +4,22 @@
 /**
  * The locale code that templates in this source code are written in.
  */
-export const sourceLocale = `en`;
+export const sourceLocale = 'en';
 
 /**
  * The other locale codes that this application is localized into. Sorted
  * lexicographically.
  */
 export const targetLocales = [
-  `es`,
-  `id`,
+  'es',
+  'id',
 ];
 
 /**
  * All valid project locale codes. Sorted lexicographically.
  */
 export const allLocales = [
-  `en`,
-  `es`,
-  `id`,
+  'en',
+  'es',
+  'id',
 ];
--- src/generated/locales/id.js
+++ src/generated/locales/id.js
@@ -1,47 +1,43 @@
+// Do not modify this file by hand!
+// Re-generate this file by running lit-localize
 
-    // Do not modify this file by hand!
-    // Re-generate this file by running lit-localize
+import { str } from '@lit/localize';
 
-    
-    import {str} from '@lit/localize';
+/* eslint-disable no-irregular-whitespace */
 
-    /* eslint-disable no-irregular-whitespace */
-    /* eslint-disable @typescript-eslint/no-explicit-any */
-
-    export const templates = {
-      's0493667c0cd0c5ff': `Ceritakan kisahmu ke semua orang.`,
-'s11b2e9ecce28690c': `Nama Pengguna`,
-'s14420b395f4e4721': `Bahasa :`,
-'s1d6075382a80bc94': str`Minimal panjang password adalah ${0}.`,
-'s1f30f2eaf1e38fb9': `Cerita Baru`,
-'s2907bf0d633436e4': `Sebuah text`,
-'s2c15b9b13a3e2a80': `Daftar`,
-'s2df075aface0dab8': `Bagikan`,
-'s2e826a0f0a885cc7': `Kisah semua orang`,
-'s2f8c5784d438537d': str`Seluruh hak cipta © ${0} milik Yusril A. P.`,
-'s374b7ba47861c79a': `Punya akun?`,
-'s3d93f9ea0c5a13fa': `Gambar`,
-'s417b90913e05bc17': `Pratinjau`,
-'s487f1ce1eecbddda': `Ceritakan Kisahmu...`,
-'s4b03071183e48d13': `Ceritakan dengan kita kisahmu`,
-'s4eac06443983c80c': `Tentang Pengembang`,
-'s5578f229c2113e87': `Ceritakan Kisah Baru`,
-'s5c61ffad8f9de079': str`Attribut "name" harus diimplementasikan pada elemen ${0}`,
-'s6abb1cd87fe0114e': `Home`,
-'s6ffd35dd41672c31': str`Attribut "url" harus diimplementasikan pada elemen ${0}`,
-'s73bb022c2937ec69': `Memuat ...`,
-'s7c3a649c7cab4bc9': str`Attribut "inputId" harus diimplementasikan pada elemen ${0}`,
-'sa031c3b6e1b9b32b': `Perlihatkan gambar kisahmu...`,
-'sa70c74939dba1821': `Email kamu tidak sesuai.`,
-'saf4fef23f47fc4a2': `Kisah`,
-'sb64127e4e4f0b575': `Keluar`,
-'sb8d24cf4c2a35b22': `Bagikan gambar kisahmu`,
-'sbf91c9859d0713c6': `Tolong isikan nama penggunamu.`,
-'sc169ca295b22de6c': `Terlihat Bagus`,
-'sc7b8edcd8c115f7c': `Gambar kisahmu terlihat menakjubkan !`,
-'sc8da3cc71de63832': `Masuk`,
-'sd4a5ecc9d35d60b3': `Belum punya akun?`,
-'sddae88563a4deba1': `Kisahmu tampak menakjubkan !`,
-'sffb3ebf3c9e204af': `Mulai Sekarang`,
-    };
-  
+export const templates = {
+  s0493667c0cd0c5ff: 'Ceritakan kisahmu ke semua orang.',
+  s11b2e9ecce28690c: 'Nama Pengguna',
+  s14420b395f4e4721: 'Bahasa :',
+  s1d6075382a80bc94: str`Minimal panjang password adalah ${0}.`,
+  s1f30f2eaf1e38fb9: 'Cerita Baru',
+  s2907bf0d633436e4: 'Sebuah text',
+  s2c15b9b13a3e2a80: 'Daftar',
+  s2df075aface0dab8: 'Bagikan',
+  s2e826a0f0a885cc7: 'Kisah semua orang',
+  s2f8c5784d438537d: str`Seluruh hak cipta © ${0} milik Yusril A. P.`,
+  s374b7ba47861c79a: 'Punya akun?',
+  s3d93f9ea0c5a13fa: 'Gambar',
+  s417b90913e05bc17: 'Pratinjau',
+  s487f1ce1eecbddda: 'Ceritakan Kisahmu...',
+  s4b03071183e48d13: 'Ceritakan dengan kita kisahmu',
+  s4eac06443983c80c: 'Tentang Pengembang',
+  s5578f229c2113e87: 'Ceritakan Kisah Baru',
+  s5c61ffad8f9de079: str`Attribut "name" harus diimplementasikan pada elemen ${0}`,
+  s6abb1cd87fe0114e: 'Home',
+  s6ffd35dd41672c31: str`Attribut "url" harus diimplementasikan pada elemen ${0}`,
+  s73bb022c2937ec69: 'Memuat ...',
+  s7c3a649c7cab4bc9: str`Attribut "inputId" harus diimplementasikan pada elemen ${0}`,
+  sa031c3b6e1b9b32b: 'Perlihatkan gambar kisahmu...',
+  sa70c74939dba1821: 'Email kamu tidak sesuai.',
+  saf4fef23f47fc4a2: 'Kisah',
+  sb64127e4e4f0b575: 'Keluar',
+  sb8d24cf4c2a35b22: 'Bagikan gambar kisahmu',
+  sbf91c9859d0713c6: 'Tolong isikan nama penggunamu.',
+  sc169ca295b22de6c: 'Terlihat Bagus',
+  sc7b8edcd8c115f7c: 'Gambar kisahmu terlihat menakjubkan !',
+  sc8da3cc71de63832: 'Masuk',
+  sd4a5ecc9d35d60b3: 'Belum punya akun?',
+  sddae88563a4deba1: 'Kisahmu tampak menakjubkan !',
+  sffb3ebf3c9e204af: 'Mulai Sekarang',
+};
